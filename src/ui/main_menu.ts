1312f24f3a8d81b50e7e629a05fe8511c02cf11c
Mart <mart_lepanen@hotmail.com>
2023-12-29 21:25:42 +0200

    Add hbox!

--- src/ui/main_menu.ts
+++ src/ui/main_menu.ts
@@ -1,7 +1,8 @@
 
 import * as Phaser from "phaser";
-import { vBoxContainer } from "../utility/containers";
+import { hBoxContainer, vBoxContainer } from "../utility/containers";
 import { SPRITES } from "../utility/constants";
+import { easeInOutBack } from "../utility/easing";
 
 export default class MainMenu extends Phaser.Scene {
 
@@ -34,13 +35,48 @@ export default class MainMenu extends Phaser.Scene {
             .setAlpha(1)
             .setScale(0.5, 0.5);
 
+        const quit2 = this.add
+            .sprite(0, 0, SPRITES.BUTTONS.QUIT)
+            .setInteractive()
+            .setAlpha(1)
+            .setScale(0.5, 0.5);
+        const quit3 = this.add
+            .sprite(0, 0, SPRITES.BUTTONS.QUIT)
+            .setInteractive()
+            .setAlpha(1)
+            .setScale(0.5, 0.5);
+        const quit4 = this.add
+            .sprite(0, 0, SPRITES.BUTTONS.QUIT)
+            .setInteractive()
+            .setAlpha(1)
+            .setScale(0.5, 0.5);
+
+        quit4.on('pointerover', () => {
+            this.tweens.add({
+                targets: quit4,
+                y: quit4.y - 6,
+                ease: easeInOutBack,
+                duration: 200,
+            });
+        })
+        quit4.on('pointerout', () => {
+            this.tweens.add({
+                targets: quit4,
+                y: quit4.y + 6,
+                ease: easeInOutBack,
+                duration: 200,
+            });
+        })
+
         var labelText = "Space Interceptor"
         var textStyle = { fontSize: "48px", fontFamily: 'FFFForward', color: '#000000' }; // Set your desired text style
         var text = this.add.text(0, 0, labelText, textStyle);
+        const hbox = hBoxContainer({ children: [quit2, quit3, quit4], scene: this })
 
-        const vbox = vBoxContainer({ children: [text, play, quit], scene: this })
+        const vbox = vBoxContainer({ children: [text, play, hbox, quit], scene: this })
         vbox.x = this.cameras.main.width / 2 - vbox.width / 2
         vbox.y = this.cameras.main.height / 2 - vbox.height / 2
+
         var graphics = this.add.graphics();
         graphics.lineStyle(2, 0x00ff00); // Green color, 2-pixel line
         graphics.strokeRect(vbox.x, vbox.y, vbox.width, vbox.height);
--- src/utility/containers.ts
+++ src/utility/containers.ts
@@ -1,7 +1,7 @@
 import * as Phaser from "phaser";
 
 // Define the possible game object types
-type GameObjectType = Phaser.GameObjects.Sprite | Phaser.GameObjects.Text;
+type GameObjectType = Phaser.GameObjects.Sprite | Phaser.GameObjects.Text | Phaser.GameObjects.Container
 
 type HBoxBindings = {
   x?: string;
@@ -11,86 +11,37 @@ type HBoxBindings = {
   scene: Phaser.Scene;
 };
 
-const PADDING = 20;
-
-export const hBoxContainer = (bindings: HBoxBindings) => {
-  //   const x = bindings.scene.cameras.main.width / 2;
-  //   const y = bindings.scene.cameras.main.height / 2;
-  const gap = bindings.gap ? bindings.gap : 20;
-  const container = bindings.scene.add.container(0, 0);
-
-  let tallestValue = 0;
-  let tallestElement = bindings.children[0];
-
-  let width = PADDING;
-
-  bindings.children.forEach((go: GameObjectType, index: number) => {
-    container.add(go);
-
-    console.log("Called me ", go);
-
-    const myWidth = go.displayWidth;
-    width += gap + myWidth;
-
-    if (go.displayHeight > tallestValue) {
-      tallestValue = go.displayHeight;
-      tallestElement = go;
-    }
-
-    if (index > 0) {
-      //   go.setX(
-      //     PADDING + bindings.children[index - 1].displayWidth + myWidth / 2 + gap
-      //   );
-
-      go.setX(width - myWidth + gap);
-    } else if (index + 1 === bindings.children.length) {
-      go.setX(width - myWidth);
-    } else {
-      go.setX(0 + PADDING + myWidth / 2);
-    }
-  });
-  container.height = tallestValue;
-
-  bindings.children.forEach((e) => {
-    e.y = tallestValue / 2;
-  });
-  container.x = (bindings.scene.cameras.main.width - width) / 2;
-  container.y = (bindings.scene.cameras.main.height - tallestValue) / 2;
-
-  var graphics = bindings.scene.add.graphics();
-  graphics.lineStyle(2, 0x00ff00); // Green color, 2-pixel line
-  graphics.strokeRect(container.x, container.y, width, tallestValue);
-};
-
 export const vBoxContainer = (bindings: HBoxBindings) => {
-  // const x = bindings.scene.cameras.main.width / 2;
-  // const y = bindings.scene.cameras.main.height / 2;
   const gap = bindings.gap ? bindings.gap : 20;
   const container = bindings.scene.add.container(0, 0);
 
 
-  if(bindings.children.length === 0){
+  if (bindings.children.length === 0) {
     return
   }
 
   let containerHeight = 0
   let containerWidth = 0
 
-  if(bindings.children.length === 1){
+  if (bindings.children.length === 1) {
     const go = bindings.children[0]
-    go.setOrigin(0, 0)
+    if ("setOrigin" in go) {
+      go.setOrigin(0, 0)
+    }
     container.add(go);
     container.width = go.displayWidth
     container.height = go.displayHeight
   } else {
     bindings.children.forEach((go: GameObjectType, index: number) => {
-      go.setOrigin(0, 0)
+      if ("setOrigin" in go) {
+        go.setOrigin(0, 0)
+      }
       container.add(go);
       containerHeight += go.displayHeight
       containerHeight += gap
       containerWidth = Math.max(containerWidth, go.displayWidth)
 
-      if(index === 0){
+      if (index === 0) {
         go.y = 0
       } else {
         const prev = bindings.children[index - 1]
@@ -100,6 +51,56 @@ export const vBoxContainer = (bindings: HBoxBindings) => {
 
     bindings.children.forEach((go) => {
       go.x = containerWidth / 2 - go.displayWidth / 2
+    })
+
+  }
+
+  container.height = containerHeight;
+  container.width = containerWidth
+  return container
+};
+
+export const hBoxContainer = (bindings: HBoxBindings) => {
+  const gap = bindings.gap ? bindings.gap : 20;
+  const container = bindings.scene.add.container(0, 0);
+
+
+  if (bindings.children.length === 0) {
+    return
+  }
+
+  let containerHeight = 0
+  let containerWidth = 0
+
+  if (bindings.children.length === 1) {
+    const go = bindings.children[0]
+    if("setOrigin" in go){
+      go.setOrigin(0, 0)
+    }
+    container.add(go);
+    container.width = go.displayWidth
+    container.height = go.displayHeight
+  } else {
+      containerWidth += gap
+    bindings.children.forEach((go: GameObjectType, index: number) => {
+      if("setOrigin" in go){
+        go.setOrigin(0, 0)
+      }
+      container.add(go);
+      containerWidth += go.displayWidth
+      containerWidth += gap
+      containerHeight = Math.max(containerHeight, go.displayHeight)
+
+      if (index === 0) {
+        go.x = gap
+      } else {
+        const prev = bindings.children[index - 1]
+        go.x = prev.x + prev.displayWidth + gap
+      }
+    })
+
+    bindings.children.forEach((go) => {
+      go.y = containerHeight / 2 - go.displayHeight / 2
     })
 
   }
