fa5677eeaa30e7bfd0105a534b195484eb692b62
Mart <mart_lepanen@hotmail.com>
2024-01-06 12:38:40 +0200

    Add signals

---
+++ src/utility/signals.ts
@@ -0,0 +1,33 @@
+// I want to be able to add a listener
+// To send a signal
+// And call each listener with that signal
+
+type Listener = (payload?: any[]) => void;
+
+const signals = () => {
+  let listeners: { listener: Listener; signal: string }[] = [];
+  return () => {
+    const addListener = (signalName: string, l: Listener) => {
+      listeners.push({ signal: signalName, listener: l });
+    };
+
+    const removeListener = (signalName: string, l: Listener) => {
+      listeners.push({ signal: signalName, listener: l });
+      listeners = listeners.filter(({ signal, listener }) => {
+        return !(signal === signalName && listener === l);
+      });
+    };
+
+    const emit = (signalName: string, payload?: any[]) => {
+      listeners.forEach(({ listener, signal }) => {
+        //
+        if (signalName === signal) {
+          listener(payload);
+        }
+      });
+    };
+    return { emit, addListener, removeListener };
+  };
+};
+
+export const Signals = signals();
